9d8d96f82c9941a79a298f756a77496aea0b38d6
mensahbenjamin036 <61154796+mensahbenjamin036@users.noreply.github.com>
2020-03-08 17:32:11 +0800

    Add files via upload

---
+++ Ecg.py
@@ -0,0 +1,19 @@
+
+units=int(input("Number of unit consumed: "))
+if(units>0 and units<=100):
+     payAmount=units*1.5
+     fixedcharge=25.00
+elif(units>100 and units<=200):
+        payAmount=(100*1.5)+(units-100)*2.5
+        fixedcharge=50.00
+elif(units>200 and units<=300):
+        payAmount=(100*1.5)+(200-100)*2.5+(units-200)*4
+        fixedcharge=50.00
+elif(units>300):
+        payAmount=2500
+        fixedcharge=75.00
+else:
+        payAmount=0
+        
+Total= payAmount+fixedcharge
+print("\nElecticity bill pay=%.2f: " %Total)
